69d2f34ead410a0cf1f881f0cca4757b7ed12ab2
Ayesha Khan <hamzakhanyousafzai55559094@gmail.com>
2023-02-14 11:36:30 +0500

    new changes

--- src/Pages/Investment/Risk.js
+++ src/Pages/Investment/Risk.js
@@ -10,6 +10,9 @@ import SkipButton from "../../components/subComponents/SkipButton";
 import Progress from "../../components/progress/Progress";
 
 const Risk = () => {
+  const handleClick= ()=>{
+    alert ("button is working")
+  }
   return (
     <Container>
       <div className="p-md-2">
@@ -30,14 +33,17 @@ const Risk = () => {
                 <Col className="">
                   <button className="btn2-cash p-3 w-100 ">Low </button>
                 </Col>
-                <Col><button className="btn-cash p-3 w-100  ">Medium </button></Col>
+                <Col>
+                  <button className="btn-cash p-3 w-100  ">Medium </button>
+                </Col>
                 <Col>
                   <button className="btn2-cash p-3 w-100 ">High </button>
                 </Col>
               </Row>
             </div>
             <div className="risk-value pt-2">
-            <strong style={{ color: "purple", fontSize: "30px" }}>4</strong>/10
+              <strong style={{ color: "purple", fontSize: "30px" }}>4</strong>
+              /10
             </div>
             <div className="risk-values pt-2">
               <Row>
@@ -46,20 +52,26 @@ const Risk = () => {
                 <Col className="text-end">High risk</Col>
               </Row>
             </div>
-            <div className="risk-progress pt-5">
-              <Progress/>
+            <div className="range pt-3">
+              <input type="range" className="form-range" id="customRange1" />
             </div>
-            <div className="risk-para  fs-7">
-              <p>Typically, Most of tis portfolio will be invested in bonds, but almost half of the portfolio
-will be invested in equities and other risky assets over certain periods.</p>
+            <div className="risk-progress pt-3">
+              <Progress />
+            </div>
+            <div className="risk-para  ">
+              <p>
+                Typically, Most of tis portfolio will be invested in bonds, but
+                almost half of the portfolio will be invested in equities and
+                other risky assets over certain periods.
+              </p>
             </div>
           </div>
-          <div className="Continue mt-5 pt-5  ">
+          <div className="Continue mt-5 mb-5 ">
             <div className="skip pb-3">
               <SkipButton value={"Skip"} />
             </div>
             <Link to="/home">
-              <CustomButtons value={"Continue"} />
+              <CustomButtons onClick={handleClick} value={"Continue"} />
             </Link>
           </div>{" "}
         </Col>
